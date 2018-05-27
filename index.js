69bfa982e7408a5dc4e2b03ae6c75d5deac5d9a1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:31 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
-import reduceWithValueKey from "@unction/reducewithvaluekey"
-import mergeRight from "@unction/mergeright"
-import fresh from "@unction/fresh"
-import attach from "@unction/attach"
-import type from "@unction/type"
-import isType from "@unction/istype"
-import {join} from "most"
-
-import type {FunctorType} from "types"
-import type {UnaryFunctionType} from "types"
-import type {KeyType} from "types"
-
-export default function flatten (functor: FunctorType): FunctorType {
+import reduceWithValueKey from "@unction/reducewithvaluekey";
+import mergeRight from "@unction/mergeright";
+import fresh from "@unction/fresh";
+import attach from "@unction/attach";
+import type from "@unction/type";
+import isType from "@unction/istype";
+import { join } from "most";
+export default function flatten(functor) {
   if (isType("Stream")(functor)) {
-    return join(functor)
+    return join(functor);
   }
 
-  return reduceWithValueKey(
-    (accumulated: FunctorType): UnaryFunctionType =>
-      (value: mixed): UnaryFunctionType =>
-        (key: KeyType): FunctorType => {
-          if (isType(type(functor))(value)) {
-            return mergeRight(accumulated)(value)
-          }
+  return reduceWithValueKey(accumulated => value => key => {
+    if (isType(type(functor))(value)) {
+      return mergeRight(accumulated)(value);
+    }
 
-          return attach(key)(value)(accumulated)
-        }
-  )(
-    fresh(functor)
-  )(
-    functor
-  )
+    return attach(key)(value)(accumulated);
+  })(fresh(functor))(functor);
 }
