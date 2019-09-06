c6c703829bf65e8d3cdf9d4366edfa50f18f6c4e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:45 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,39 @@
+
+import {from} from "most";
+import streamSatisfies from "@unction/streamsatisfies";
+
+import flatten from "./index.ts";
+
+test("Array, two dimensons", () => {
+  expect(flatten([["a", "b"], ["c", "d"]])).toEqual(["a", "b", "c", "d"]);
+});
+
+test("Array, mixed dimensons", () => {
+  expect(flatten(["a", "b", ["c", "d"]])).toEqual(["a", "b", "c", "d"]);
+});
+
+test("Stream", done => {
+  streamSatisfies([
+    "a",
+    "b",
+    "c",
+    "d",
+  ])(
+    (given) => (expected) => expect(given).toBe(expected)
+  )(
+    doesNotThrow
+  )(
+    ({length}) => (size) => {
+      expect(length).toBe(size);
+
+      done();
+    }
+  )(
+    flatten(
+      from([
+        from(["a", "b"]),
+        from(["c", "d"]),
+      ])
+    )
+  );
+});
